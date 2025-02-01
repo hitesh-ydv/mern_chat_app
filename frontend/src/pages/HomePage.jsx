8a41a1fe5f499ba0639d899b34046bbfe744c70d
Hitesh Yadav <hiteshyadav124001@gmail.com>
2025-02-01 12:09:54 +0530

    first commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,25 +3,25 @@ import { useChatStore } from "../store/useChatStore";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
-import { Fade } from "react-reveal";
+
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <Fade>
+    <Fade>
+      <div className="h-screen bg-base-200">
+        <div className="flex items-center justify-center pt-20 px-4">
+          <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
             <div className="flex h-full rounded-lg overflow-hidden">
               <Sidebar />
 
               {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
             </div>
-          </Fade>
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 export default HomePage;
